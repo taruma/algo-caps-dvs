0e90ad08738bfd847b526aff4b0d72505f31650d
taruma sakti <hi@taruma.info>
2022-10-29 22:21:53 +0700

    refresh

--- app.R
+++ app.R
@@ -1,51 +1,113 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# APLIKASI SHINY UNTUK DV CAPSTONE DISURUH SAMA ALGOSCAM
+
+require(readr)
+
+library(dplyr)
 
 library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(DT)
+
+## PREP ----
+
+eurovision <- readr::read_csv('data/eurovision.csv')
+
+factor_columns <- c(
+  "host_city", "event", "host_country", "section", "artist_country"
+)
+
+integer_columns <- c(
+  "year", "running_order", "total_points", "rank"
+)
+
+eurovision <- eurovision |> 
+  mutate(across(all_of(factor_columns), as.factor)) |> 
+  mutate(across(all_of(integer_columns), as.integer))
+
+## UI ----
+
+header <- dashboardHeader(
+  # https://github.com/RinteRface/shinydashboardPlus/issues/6
+  title = tagList(
+    span(class = "logo-lg", "Eurovision Song Contest"),
+    icon("play")
+  ),
+  titleWidth = 300,
+  controlbarIcon = icon("gears", class = "fa")
+)
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Overview",
+      tabName = "tabOverview",
+      icon = icon("layer-group", class = "fa"), selected = TRUE
+    ),
+    menuItem("Explore",
+      tabName = "tabExplore",
+      icon = icon("magnifying-glass-chart", class = "fa")
+    ),
+    menuItem("Dataset",
+      tabName = "tabDataset",
+      icon = icon("table", class = "fa")
+    ),
+    menuItem("Notes",
+      tabName = "tabNotes",
+      icon = icon("clipboard", class = "fa")
+    ),
+    menuItem("Source Code",
+      href = "https://github.com/taruma/algo-caps-dvs",
+      icon = icon("github", class = "fa")
+    )
+  ),
+  collapsed = TRUE
+)
+
+tab1 <- tabItem("tabOverview")
+tab2 <- tabItem("tabExplore", h2("Hello"))
+tab3 <- tabItem(
+  "tabDataset",
+  # fluidPage(
+  includeMarkdown("md/datasetInfo.md"),
+  dataTableOutput("datasetTable")
+  # )
+)
+tab4 <- tabItem("tabNotes")
+
+
+body <- dashboardBody(
+  includeCSS("style.css"),
+  tabItems(
+    tab1, tab2, tab3, tab4
+  )
+)
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- dashboardPage(
+  header,
+  sidebar,
+  body,
+  footer = dashboardFooter(
+    left = markdown("`g00gL3 aj<~>4 K|>k`"),
+    right = markdown("`... :: <=> #{}`")
+  ),
+  skin = "blue-light",
+  title = "Eurovision Song Contest Dashboard"
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$datasetTable <- renderDataTable(
+    eurovision |>
+      select(
+        -all_of(c("event_url", "artist_url", "image_url", "country_emoji"))
+      ),
+    options = list(pageLength = 10, scrollX = "100%"),
+    filter = "top",
+    selection = "single"
+  )
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
---
+++ md/datasetInfo.md
@@ -0,0 +1,29 @@
+# TABEL DATASET
+
+Sumber: <https://www.kaggle.com/datasets/evangower/eurovision-song-contest>
+
+## Deskripsi Kolom
+
+```r
+event <- [chr] "Name of event"
+host_city <- [chr] "City hosting the event"
+year <- [num] "Year of event"
+host_country <- [chr] "Country hosting the event"
+event_url <- [chr] "Link of event"
+section <- [chr] "Round of contest"
+artist <- [chr] "Singer(s) / band name"
+song <- [chr] "Song name"
+artist_url <- [chr] "Artist link"
+image_url <- [chr] "Image link"
+artist_country <- [chr] "Country artist is from"
+country_emoji <- [chr] "Flag emoji"
+running_order <- [num] "Order of singing"
+total_points <- [num] "Points scored in event"
+rank <- [num] "Rank in event"
+rank_ordinal <- [chr] "Rank ordinal in event"
+qualified <- [logi] "Did they advance to next round?"
+winner <- [logi] "Did they win?"
+```
+
+## Tabel Data
+
